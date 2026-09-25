715ec968c6e55ded62ada0402096f9836cecaada
silaoral-34 <silaoral34@gmail.com>
2026-09-25 22:13:27 +0300

    Update grade_calculator.py

--- week2/grade_calculator.py
+++ week2/grade_calculator.py
@@ -1,1 +1,38 @@
+students = [ ]
+scores = [ ]
+
+while True:
+  student_name = input("Enter student name (or q to quit): ")
+
+if student_name == "q":
+  break 
+  
+score = float(input(Enter score: "))
+                    
+if score < 0 or score > 100:
+   print("Invalid score. Please enter a number between 0 and 100.")   
+   continue 
+                    
+if score >= 90:
+   grade = "A"
+elif score >= 80:
+   grade = "B"
+elif score >= 70:
+   grade = "C" 
+elif score >= 60:
+   grade = "D"
+else:
+   grade = "f"
+
+print(f{student_name}: {score:g}  -> {grade}")
+
+students.append(student_name)
+scores.append(score)
+
+if len (students) == 0:
+  print(" No students entered.")
+else:
+  average = sum(scores) / len(scores)
+  print(f"Total students: {len(students)}")
+  print (f"Average score: {average:.2f})
 
